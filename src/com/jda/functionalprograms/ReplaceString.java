6db7bd30a46de94c0953746e28d8d2191d5b67d4
Dhiraj-kanake <kanake.dhiraj@gmail.com>
2018-07-05 17:21:11 +0530

    Functional Programs

--- src/com/jda/functionalprograms/ReplaceString.java
+++ src/com/jda/functionalprograms/ReplaceString.java
@@ -1,6 +1,5 @@
 package com.jda.functionalprograms;
 
-import java.util.Scanner;
 import com.jda.utility.Utility;
 public class ReplaceString {
   public static void main(String args[])
@@ -9,5 +8,6 @@ public class ReplaceString {
 	  String Template = "Hello <<UserName>> ,How are you?";
        String UserName = utility.ScanString();
 	  String output =utility.StringReplace(UserName,Template);
+	  System.out.println(output);
   }
 }
