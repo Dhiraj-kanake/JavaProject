378886facfa2d7b580c3903ee38af4a0a7b16a83
Dhiraj-kanake <kanake.dhiraj@gmail.com>
2018-07-12 12:35:19 +0530

    functionaPrograms

--- src/com/jda/functionalprograms/WindChill.java
+++ src/com/jda/functionalprograms/WindChill.java
@@ -3,11 +3,14 @@ package com.jda.functionalprograms;
 import com.jda.utility.Utility;
 
 public class WindChill {
-public static void main(double args[])
+public static void main(String args[])
 {
+	double arr[]=new double[2];
+	arr[0] = Double.parseDouble(args[0]);
+	arr[1] = Double.parseDouble(args[1]);
 	Utility utility = new Utility();
- double windchill = utility.findWindChill(args);
- System.out.println(windchill);
+ double windchill = utility.findWindChill(arr);
+ System.out.println("windchill : "+windchill);
 }
 
 }
