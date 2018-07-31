393e318c44b902ce5ee088ef65747f933c3300b7
Dhiraj <Dhiraj.Kanake@jda.com>
2018-07-31 12:08:06 +0000

    shuffle cards

--- src/com/jda/objectOrientedPrograms/deckOfCards.java
+++ src/com/jda/objectOrientedPrograms/deckOfCards.java
@@ -1,7 +1,11 @@
 package com.jda.objectOrientedPrograms;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.jda.utility.Utility;
@@ -9,9 +13,8 @@ import com.jda.utility.Utility;
 public class deckOfCards {
 public static void main(String args[])
 {
-	Utility utility = new Utility();
-	String arr[]={"2D","3D","4D","5D","6D","7D","8D","9D","10D","JACK_D","QUEEN_D","KING_D","ACE_D","2C","3C","4C","5C","6C","7C","8C","9C","10C","JACK_C","QUEEN_C","KING_C","ACE_C","2H","3H","4H","5H","6H","7H","8H","9H","10H","JACK_H","QUEEN_H","KING_H","ACE_H","2S","3S","4S","5S","6S","7S","8S","9S","10S","JACK_S","QUEEN_S","KING_S","ACE_S"};
-
+	String arr[]={"2D","2H","2S","2C","3D","3H","3S","3C","4D","4H","4S","4C","5D","5H","5S","5C","6D","6H","6S","6C","7D","7H","7S","7C","8D","8H","8S","8C","9D","9H","9S","9C","10D","10H","10S","10C","JACK_D","JACK_H","JACK_S","JACK_C","QUEEN_D","QUEEN_H","QUEEN_S","QUEEN_C","KING_D","KING_H","KING_S","KING_C","ACE_D","ACE_H","ACE_S","ACE_C",};
+	String arr2[]={"2D","2H","2S","2C","3D","3H","3S","3C","4D","4H","4S","4C","5D","5H","5S","5C","6D","6H","6S","6C","7D","7H","7S","7C","8D","8H","8S","8C","9D","9H","9S","9C","10D","10H","10S","10C","JACK_D","JACK_H","JACK_S","JACK_C","QUEEN_D","QUEEN_H","QUEEN_S","QUEEN_C","KING_D","KING_H","KING_S","KING_C","ACE_D","ACE_H","ACE_S","ACE_C",};
 	 Random rnd = ThreadLocalRandom.current();
 	 for(int repeat=0;repeat<2;repeat++)
 	 {
@@ -43,6 +46,39 @@ public static void main(String args[])
 		 }
 		 System.out.println();
 	 }
+	 
+	 int sortedIndex[]=new int[36];
+	 int indexArray=0;
+	 for(int i=0;i<4;i++)
+	 {
+		 System.out.println("Player "+(i+1));
+		 HashMap<Integer,String> hMap=new HashMap<>();
+		 for(int j=0;j<9;j++)
+		 {
+			 
+			 for(int indexOfSortedArray=0;indexOfSortedArray<52;indexOfSortedArray++)
+			 {
+				 if(arr2[indexOfSortedArray].equals(people[i][j]))
+				 {
+					 sortedIndex[indexArray] = indexOfSortedArray;
+					 hMap.put(indexOfSortedArray,people[i][j] );
+					 indexArray+=1;
+				 }
+			 }
+			 
+		 }
+		 Map<Integer,String> tMap=new TreeMap<>(hMap);
+		 Iterator itr=tMap.entrySet().iterator();
+		 while(itr.hasNext())
+		 {
+			 System.out.print("  "+itr.next());
+		 }
+		 System.out.println();
+	
+		 
+	 }
+	 System.out.println("index : "+Arrays.toString(sortedIndex));
+	 
 	 
    // System.out.println("arrayt: "+Arrays.toString(arr));
 }
